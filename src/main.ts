ff4365a64b4a209c88b710ac31b9341817224dad
Daniel Peñaloza <juandanielpenalozabrito@gmail.com>
2025-08-24 15:34:47 -0500

    Revert cors changes

--- src/main.ts
+++ src/main.ts
@@ -10,13 +10,19 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const configService = app.get(ConfigService);
   const port = configService.get<number>('APP_PORT', 4000);
+  const frontendUrl = configService.get<string>('FRONTEND_URL', 'http://localhost:4200');
 
   app.useGlobalPipes(validationPipeConfig);
   app.useGlobalFilters(new HttpExceptionFilter());
   app.useGlobalInterceptors(new ResponseInterceptor ());
 
-  // CORS configuration
-  app.enableCors();
+  // Enhanced CORS configuration
+  app.enableCors({
+    origin: frontendUrl,
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    allowedHeaders: 'Content-Type, Accept, Authorization',
+    credentials: true,
+  });
 
   // Swagger configuration
   const config = new DocumentBuilder()
